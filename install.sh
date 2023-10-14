bdabb2466cc1da55513a12dab6d703c635277e1a
0xPwn1 <111459230+SegmaSec@users.noreply.github.com>
2023-10-14 13:49:29 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
-sudo mv Script.py /usr/local/bin/JWT-Token
-sudo chmod +x /usr/local/bin/JWT-Token
-sudo rm -rf ../JWT-Token
+sudo mv Script.py /usr/local/bin/jwt-token
+sudo chmod +x /usr/local/bin/jwt-token
+sudo rm -rf ../JWT-Token && cd ..
 
 echo "JWT-Token is Installed !!!"
